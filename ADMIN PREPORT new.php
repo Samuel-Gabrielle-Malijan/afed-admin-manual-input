414c508990d373318dcca5d7979a25ac4d0e8a05
Anjoh-Sarte <anjoh_sarte@dlsu.edu.ph>
2018-11-02 11:52:15 +0800

    Made reusable codes

--- ADMIN PREPORT new.php
+++ ADMIN PREPORT new.php
@@ -83,7 +83,13 @@ $result=mysqli_query($dbc,$query);
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+<script src = "https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"> 
+    </script>
 
+     <script> $(function(){
+        $('#load').load('navBar.php');
+     });
+    </script>
 </head>
 
 <body>
