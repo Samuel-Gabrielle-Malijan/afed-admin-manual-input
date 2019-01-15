a8d66780e338701c925b366be5e46e814f4bdc31
Anjoh-Sarte <anjoh_sarte@dlsu.edu.ph>
2019-01-16 01:18:36 +0800

    Fixed falp manual

--- ADMIN FALP manual.php
+++ ADMIN FALP manual.php
@@ -27,7 +27,13 @@
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <script src = "https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"> 
+    </script>
 
+     <script> $(function(){
+     	$('#load').load('navBar.php');
+     });
+    </script>
 </head>
 
 <?php
@@ -201,6 +207,7 @@ $success = null;
 
                $haddress = $_POST['haddress'];
                $honum = $_POST['honum'];
+               $campus = $_POST['campus'];
                //set dates
 
                $birthdate = $bYear . "-" . $bMonth . "-" . $bDay;
@@ -216,8 +223,8 @@ $success = null;
                      $baddress = $_POST['baddress'];
 
 
-                     $query1 = "INSERT INTO MEMBER (MEMBER_ID, FIRSTNAME, LASTNAME,CIV_STATUS, MIDDLENAME,SEX, BIRTHDATE ,DATE_HIRED, HOME_NUM, BUSINESS_NUM, HOME_ADDRESS, BUSINESS_ADDRESS, DEPT_ID,USER_STATUS,MEMBERSHIP_STATUS,DATE_APPLIED,DATE_APPROVED,EMP_ID_APPROVE) 
-                                          VALUES ('{$idNum}','{$fName}','{$lName}',{$civStat},'{$mName}',{$sex},'{$birthdate}','{$datehired}',{$honum},{$bunum},'{$haddress}','{$baddress}',{$dept},1,2,'{$dateappl}','{$dateapp}','{$emp_ID}')";
+                     $query1 = "INSERT INTO MEMBER (MEMBER_ID, FIRSTNAME, LASTNAME,CIV_STATUS, MIDDLENAME,SEX, BIRTHDATE ,DATE_HIRED, HOME_NUM, BUSINESS_NUM, HOME_ADDRESS, BUSINESS_ADDRESS, DEPT_ID,USER_STATUS,MEMBERSHIP_STATUS,DATE_APPLIED,DATE_APPROVED,EMP_ID_APPROVE,CAMPUS) 
+                                          VALUES ('{$idNum}','{$fName}','{$lName}',{$civStat},'{$mName}',{$sex},'{$birthdate}','{$datehired}',{$honum},{$bunum},'{$haddress}','{$baddress}',{$dept},1,2,'{$dateappl}','{$dateapp}','{$emp_ID}','{$campus}')";
 
                       $result = mysqli_query($dbc,$query1);
 
@@ -232,7 +239,7 @@ $success = null;
                     $bunum = $_POST['bunum'];
 
                     $query1 = "INSERT INTO MEMBER (MEMBER_ID, FIRSTNAME, LASTNAME,MIDDLENAME,CIV_STATUS,SEX, BIRTHDATE ,DATE_HIRED, HOME_NUM, BUSINESS_NUM, HOME_ADDRESS, 
-                          DEPT_ID,USER_STATUS,MEMBERSHIP_STATUS,DATE_APPLIED,DATE_APPROVED,EMP_ID_APPROVE) VALUES ('{$idNum}','{$fName}','{$lName}','{$mName}',{$civStat},{$sex},'{$birthdate}','{$datehired}',{$honum},{$bunum},'{$haddress}',{$dept},1,2,'{$dateappl}','{$dateapp}','{$emp_ID}')"; 
+                          DEPT_ID,USER_STATUS,MEMBERSHIP_STATUS,DATE_APPLIED,DATE_APPROVED,EMP_ID_APPROVE,CAMPUS) VALUES ('{$idNum}','{$fName}','{$lName}','{$mName}',{$civStat},{$sex},'{$birthdate}','{$datehired}',{$honum},{$bunum},'{$haddress}',{$dept},1,2,'{$dateappl}','{$dateapp}','{$emp_ID}','{$campus}')"; 
 
 
                       $result = mysqli_query($dbc,$query1); 
@@ -248,7 +255,7 @@ $success = null;
                     $baddress = $_POST['baddress'];
 
                     $query1 = "INSERT INTO MEMBER (MEMBER_ID, FIRSTNAME, LASTNAME, CIV_STATUS, MIDDLENAME, SEX, BIRTHDATE, DATE_HIRED, HOME_NUM, HOME_ADDRESS, BUSINESS_ADDRESS, 
-                          DEPT_ID,USER_STATUS,MEMBERSHIP_STATUS,DATE_APPLIED,DATE_APPROVED,EMP_ID_APPROVE) VALUES ('{$idNum}','{$fName}','{$lName}',{$civStat}, '{$mName}',{$sex},'{$birthdate}','{$datehired}',{$honum},'{$haddress}','{$baddress}',{$dept},1,2,'{$dateappl}','{$dateapp}','{$emp_ID}')"; 
+                          DEPT_ID,USER_STATUS,MEMBERSHIP_STATUS,DATE_APPLIED,DATE_APPROVED,EMP_ID_APPROVE,CAMPUS) VALUES ('{$idNum}','{$fName}','{$lName}',{$civStat}, '{$mName}',{$sex},'{$birthdate}','{$datehired}',{$honum},'{$haddress}','{$baddress}',{$dept},1,2,'{$dateappl}','{$dateapp}','{$emp_ID}','{$campus}')"; 
 
 
                      $result = mysqli_query($dbc,$query1); 
@@ -264,8 +271,8 @@ $success = null;
 
                else { //when Business address and Business Number is empty
 
-                    $query1 = "INSERT INTO MEMBER (MEMBER_ID, FIRSTNAME, LASTNAME, CIV_STATUS,  MIDDLENAME,SEX, BIRTHDATE ,DATE_HIRED, HOME_NUM, HOME_ADDRESS, DEPT_ID, USER_STATUS,MEMBERSHIP_STATUS,DATE_APPLIED,DATE_APPROVED,EMP_ID_APPROVE) 
-                        VALUES ('{$idNum}','{$fName}','{$lName}',{$civStat}, '{$mName}','{$sex}','{$birthdate}','{$datehired}','{$honum}','{$haddress}',{$dept},1,2,'{$dateappl}','{$dateapp}','{$emp_ID}')"; 
+                    $query1 = "INSERT INTO MEMBER (MEMBER_ID, FIRSTNAME, LASTNAME, CIV_STATUS,  MIDDLENAME,SEX, BIRTHDATE ,DATE_HIRED, HOME_NUM, HOME_ADDRESS, DEPT_ID, USER_STATUS,MEMBERSHIP_STATUS,DATE_APPLIED,DATE_APPROVED,EMP_ID_APPROVE,CAMPUS) 
+                        VALUES ('{$idNum}','{$fName}','{$lName}',{$civStat}, '{$mName}','{$sex}','{$birthdate}','{$datehired}','{$honum}','{$haddress}',{$dept},1,2,'{$dateappl}','{$dateapp}','{$emp_ID}','{$campus}')"; 
 
                     $result = mysqli_query($dbc,$query1); 
 
@@ -284,7 +291,7 @@ $success = null;
                 			$falpPaid = $_POST['fAmountPaid'];
 		            	}
 		                $query = "INSERT INTO loans(MEMBER_ID,LOAN_DETAIL_ID,AMOUNT,INTEREST,PAYMENT_TERMS,PAYABLE,PER_PAYMENT,APP_STATUS,LOAN_STATUS,DATE_APPLIED,DATE_APPROVED,PICKUP_STATUS,AMOUNT_PAID,EMP_ID)
-		                                    values({$_POST['idNum']},1,{$_POST['amount']},5,{$_POST['terms']},{$_POST['amount']}+{$_POST['amount']}*5/100,({$_POST['amount']}+{$_POST['amount']}*5/100)/{$_POST['terms']}/2,2,2,'{$fdateappl}','{$fdateapp}',1,{$falpPaid},{$_POST['fEmp_ID']});";
+		                          values({$_POST['idNum']},1,{$_POST['amount']},5,{$_POST['terms']},{$_POST['amount']}+{$_POST['amount']}*5/100,({$_POST['amount']}+{$_POST['amount']}*5/100)/{$_POST['terms']}/2,2,2,'{$fdateappl}','{$fdateapp}',1,{$falpPaid},{$_POST['fEmp_ID']});";
 
 		               mysqli_query($dbc,$query);
 		               
@@ -327,239 +334,9 @@ $success = null;
 <body>
 	
     <div id="wrapper">
-
-        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-            
-            <div class="navbar-header"> <!-- Logo -->
-                
-                <img src="images/I-FA Logo Edited.png" id="ifalogo">
-            
-            
-            <ul class="nav navbar-right top-nav"> <!-- Top Menu Items / Notifications area -->
-                
-                <li class="dropdown sideicons">
-
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-bell"></i> <b class="caret"></b></a>
-
-                    <ul class="dropdown-menu alert-dropdown">
-
-                        <li>
-                            <a href="#">Alert Name <span class="label label-default">Alert Badge</span></a>
-                        </li>
-
-                        <li>
-                            <a href="#">Alert Name <span class="label label-primary">Alert Badge</span></a>
-                        </li>
-
-                        <li>
-                            <a href="#">Alert Name <span class="label label-success">Alert Badge</span></a>
-                        </li>
-
-                        <li>
-                            <a href="#">Alert Name <span class="label label-info">Alert Badge</span></a>
-                        </li>
-
-                        <li>
-                            <a href="#">Alert Name <span class="label label-warning">Alert Badge</span></a>
-                        </li>
-
-                        <li>
-                            <a href="#">Alert Name <span class="label label-danger">Alert Badge</span></a>
-                        </li>
-
-                        <li class="divider"></li>
-
-                        <li>
-                            <a href="#">View All</a>
-                        </li>
-
-                    </ul>
-
-                </li>
-
-                <li class="dropdown sideicons">
-
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> Jo, Melton <b class="caret"></b></a>
-
-                    <ul class="dropdown-menu">
-
-                        <li>
-
-                            <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
-
-                        </li>
-
-                        <li class="divider"></li>
-
-                        <li>
-
-                            <a href="logout.php"><i class="fa fa-fw fa-power-off"></i> Log Out</a>
-
-                        </li>
-
-                    </ul>
-
-                </li>
-
-            </ul>
-			
-            </div>
-            <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
-            <div class="collapse navbar-collapse navbar-ex1-collapse">
-
-                <ul class="nav navbar-nav side-nav">
-
-    <li id="top">
-
-        <a href="ADMIN FALP manual.php"><i class="fa fa-gears" aria-hidden="true"></i> Add Member</a>
-
-    </li>
-
-    <!--<li>
-
-        <a href="ADMIN addaccount.php"><i class="fa fa-user-plus" aria-hidden="true"></i> Add Admin Account</a>
-
-    </li>
-
-    <li>
-
-    <a href="javascript:;" data-toggle="collapse" data-target="#applications"><i class="fa fa-wpforms" aria-hidden="true"></i>&nbsp;Applications<i class="fa fa-fw fa-caret-down"></i></a>
-
-        <ul id="applications" class="collapse">
-
-            <li>
-                <a href="ADMIN MEMBERSHIP applications.php"><i class="fa fa-id-card-o" aria-hidden="true"></i>&nbsp;&nbsp;Membership Pending Applications</a>
-            </li>
-
-            <li>
-                <a href="ADMIN FALP applications.php"><i class="fa fa-dollar" aria-hidden="true"></i>&nbsp;&nbsp;FALP Pending Applications</a>
-            </li>
-
-            <li>
-                <a href="ADMIN BANK applications.php"><i class="fa fa-institution" aria-hidden="true"></i>&nbsp;&nbsp;Bank Loan Pending Applications</a>
-            </li>
-
-            <li>
-                <a href="ADMIN HEALTHAID applications.php"><i class="fa fa-medkit" aria-hidden="true"></i>&nbsp;&nbsp;Health Aid Pending Applications</a>
-            </li>
-
-        </ul>
-
-    </li>
-
-    <li>
-
-        <a href="ADMIN LIFETIME addmember.php"><i class="fa fa-id-card-o" aria-hidden="true"></i> Add Lifetime Member</a>
-
-    </li>
-
-    <li>
-
-    <a href="javascript:;" data-toggle="collapse" data-target="#bankloans"><i class="fa fa-institution" aria-hidden="true"></i>&nbsp;Bank Loans<i class="fa fa-fw fa-caret-down"></i></a>
-
-        <ul id="bankloans" class="collapse">
-
-            <li>
-                <a href="ADMIN BANK addbank.php"><i class="fa fa-institution" aria-hidden="true"></i>&nbsp;&nbsp;Add Partner Bank</a>
-            </li>
-
-            <li>
-                <a href="ADMIN BANK editbank.php"><i class="fa fa-gears" aria-hidden="true"></i>&nbsp;&nbsp;Enable/Disable Partner Bank</a>
-            </li>
-
-            <li>
-                <a href="ADMIN BANK addplan.php"><i class="fa fa-dollar" aria-hidden="true"></i>&nbsp;&nbsp;Add Bank Loan Plan</a>
-            </li>
-
-            <li>
-                <a href="ADMIN BANK editplan.php"><i class="fa fa-gears" aria-hidden="true"></i>&nbsp;&nbsp;Enable/Disable Bank Loan Plan</a>
-            </li>
-
-        </ul>
-
-    </li>-->
-    
-            <li>
-                <a href="ADMIN MEMBERS viewmembers.php"><i class="fa fa-group" aria-hidden="true"></i>&nbsp;&nbsp;View All Members</a>
-            </li>
-
-    <li>
-
-    <a href="javascript:;" data-toggle="collapse" data-target="#loans"><i class="fa fa-money" aria-hidden="true"></i>&nbsp;On-going Loans<i class="fa fa-fw fa-caret-down"></i></a>
-
-        <ul id="loans" class="collapse">
-
-            <li>
-                <a href="ADMIN FALP viewactive.php"><i class="fa fa-dollar" aria-hidden="true"></i>&nbsp;&nbsp;FALP Loans</a>
-            </li>
-
-            <!--<li>
-                <a href="ADMIN BANK viewactive.php"><i class="fa fa-institution" aria-hidden="true"></i>&nbsp;&nbsp;Bank Loans</a>
-            </li>-->
-
-        </ul>
-
-    </li>
-
-    <li>
-
-    <a href="javascript:;" data-toggle="collapse" data-target="#dreports"><i class="fa fa-minus" aria-hidden="true"></i>&nbsp;Deduction Reports<i class="fa fa-fw fa-caret-down"></i></a>
-
-        <ul id="dreports" class="collapse">
-
-            <li>
-                <a href="ADMIN DREPORT general.php"><i class="fa fa-file-text-o" aria-hidden="true"></i>&nbsp;&nbsp;General Deductions</a>
-            </li>
-
-            <li>
-                <a href="ADMIN DREPORT detailed.php"><i class="fa fa-file-text-o" aria-hidden="true"></i>&nbsp;&nbsp;Detailed Deductions</a>
-            </li>
-
-        </ul>
-
-    </li>
-
-    <li>
-
-    <a href="javascript:;" data-toggle="collapse" data-target="#preports"><i class="fa fa-table" aria-hidden="true"></i>&nbsp;Periodical Reports<i class="fa fa-fw fa-caret-down"></i></a>
-
-        <ul id="preports" class="collapse">
-
-            <li>
-                <a href="ADMIN PREPORT completed.php"><i class="fa fa-file-text-o" aria-hidden="true"></i>&nbsp;&nbsp;Completed Loans</a>
-            </li>
-
-            <li>
-                <a href="ADMIN PREPORT new.php"><i class="fa fa-file-text-o" aria-hidden="true"></i>&nbsp;&nbsp;New Deductions</a>
-            </li>
-
-        </ul>
-
-    </li>
-
-                        <li>
-
-        <a href="ADMIN MREPORT report.php"><i class="fa fa-table" aria-hidden="true"></i> Monthly Report</a>
-
-    </li>
-
-    <li>
-
-    <a href="ADMIN FILEREPO.php"><i class="fa fa-folder-open-o" aria-hidden="true"></i>&nbsp;File Repository</i></a>
-
-    </li>
-    <!--
-    <li>
-
-        <a href="ADMIN MANAGE.php"><i class="fa fa-gears" aria-hidden="true"></i> Admin Management</a>
-
-    </li>-->
-
-</ul>
-
-            </div>
-            <!-- /.navbar-collapse -->
-        </nav>
+    		<div id ="load">
+    		</div>
+        
 
         <div id="page-wrapper">
 
@@ -705,6 +482,7 @@ $success = null;
                                                 </select>
 
 
+
                                         </div>
 
                                     </div>
@@ -774,6 +552,7 @@ $success = null;
 
 
 
+
                                         </div>
 
                                     </div>
@@ -838,9 +617,16 @@ $success = null;
                                                 } ?>>
                                                 </label>
 
+                                                 <label>
+                                                <span class="labelspan">Campus Registered<big class="req"> *</big></span>
+                                                <input type="text" class="form-control memname" placeholder="First Name" name="fName"  <?php if(isset($_POST['campus'])){
+                                                	echo "value = '{$_POST['campus']}'";
+                                                } ?>>
+                                                </label>
                                         </div>
 
                                     </div>
+
 
                                 </div>
 
